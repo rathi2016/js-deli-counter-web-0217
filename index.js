2a293518516c4275f34225349c1de37fa0fd49e0
Rathi <github email address>
2017-05-25 09:07:32 -0400

    made deli counter

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-function takeANumber(line,name){
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+function takeANumber(katzDeliLine,name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
-function nowServing(line){
-  if (!line.length) {
+
+function nowServing(katzDeliLine){
+  if (!katzDeliLine.length) {
     return "There is nobody waiting to be served!"
   }
 
-  return `Currently serving ${line.shift()}.`
+  return `Currently serving ${katzDeliLine.shift()}.`
 }
-function currentLine(line){
+
+function currentLine(katzDeliLine){
+
     const arr = []
-  if (!line.length) {
+  if (!katzDeliLine.length) {
     return "The line is currently empty."
   }
 
-  for (let i = 0, l = line.length; i < l; i++) {
-    arr.push(`${i + 1}. ${line[i]}`)
+  for (let i = 0, l = katzDeliLine.length; i < l; i++) {
+    arr.push(`${i + 1}. ${katzDeliLine[i]}`)
   }
 
   return `The line is currently: ${arr.join(', ')}`
